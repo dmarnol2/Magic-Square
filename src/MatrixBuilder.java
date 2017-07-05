d4a944adc0119a00bbccd92479cf953ee8bbb76c
dmarnol2 <dmarnol2@asu.edu>
2017-07-05 00:43:14 -0400

    Add javaDocs

--- src/MatrixBuilder.java
+++ src/MatrixBuilder.java
@@ -1,4 +1,14 @@
 
+/**
+ * This class takes the user input and creates a 
+ * 2D array, prints the array, and sums each row, column,
+ * and diagonal to test if it is a Magic Square. Uses methods
+ * fillMatrix, printMatrix, and testForMagic.
+ * 
+ * @author Dave Arnold
+ * @since 2017-06-30
+ *
+ */
 
 public class MatrixBuilder {
 	
@@ -9,10 +19,26 @@ public class MatrixBuilder {
 	int count = 0;
 	int check=0;         // conditional for while loop
 	
+	
+/**
+ * Constructor.	
+ * 
+ * @param size  Takes size based on number of elements entered
+ * 			   	and uses value to create matrix
+ * 
+ */
 	public MatrixBuilder(int size) {
 		this.squareMatrix=new int[size][size];
 		this.size=size;
 		}	
+	
+	/**
+	 * Method takes 1D array of user input and creates
+	 * 2D square matrix of size n X n
+	 * 
+	 * @param array The one dimensional array of user input
+	 * @return the matrix created
+	 */
 	
 	public int[][] fillMatrix(int[] array){
 	    for(int row=0;row<size;row++){
@@ -25,6 +51,13 @@ public class MatrixBuilder {
 	    return squareMatrix;
 	 	}
 	 
+	/**
+	 * Method prints user's square matrix
+	 * 
+	 * @param matrix the 2D matrix created from user input
+	 * @return Nothing
+	 * 
+	 */
 	public void printMatrix(int[][] matrix){
 		 for(int row=0;row<size;row++){
 			 for(int col=0;col<size;col++){
@@ -34,6 +67,17 @@ public class MatrixBuilder {
 			 }
 	 }
 	 
+	/**
+	 * Method determines the sum of each row, column, and diagonal and checks
+	 * if the requirements of Magic Square are met. Method calculates 1st row 
+	 * and uses value as a comparator for all the remaining calculations. 
+	 * Calculates rows first, then columns, then diagonals and compares each 
+	 * to comparator. Once a compare shows unequal elements, no further 
+	 * calculations are needed and the program exits immediately.
+	 * 
+	 * @param matrix the 2D matrix created from user input
+	 * @return boolean true if Magic Square, or false if not Magic Square
+	 */
 	public boolean testForMagic(int[][] matrix){
 		
 		//find value of 1st row and use to compare 
